58b97546375163cb3972de2a0e80d5d8ff84076a
Benjamin <shallifi@hotmail.com>
2022-06-10 22:59:39 -0400

    completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+
+  # getter method
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
 
 end
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+
+p snoopy.breed
